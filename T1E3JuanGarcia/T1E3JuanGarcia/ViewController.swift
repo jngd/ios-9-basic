ddc2b86d76288630e8094b61d3a5e949658ca52d
jngd <juangrdiaz@gmail.com>
2015-11-14 19:16:09 +0100

    Suject 1 finished

---
+++ T1E3JuanGarcia/T1E3JuanGarcia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  T1E3JuanGarcia
+//
+//  Created by jgd on 03/11/15.
+//  Copyright © 2015 jgd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
